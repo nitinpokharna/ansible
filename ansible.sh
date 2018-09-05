9e0bc2a01fbeef1bdd756ac63f199ed2ecd70007
nitinpokharna <40563185+nitinpokharna@users.noreply.github.com>
2018-09-05 11:44:09 +0530

    Create ansible.sh

---
+++ ansible.sh
@@ -0,0 +1,6 @@
+## Install pre-requisite packages
+sudo yum check-update; sudo yum install -y gcc libffi-devel python-devel openssl-devel epel-release
+sudo yum install -y python-pip python-wheel
+
+## Install Ansible and Azure SDKs via pip
+sudo pip install ansible[azure]
